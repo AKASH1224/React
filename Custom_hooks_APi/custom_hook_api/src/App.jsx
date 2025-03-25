147e6408682d44a0fdfdbcad00d1821ae62574ce
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2025-03-25 08:29:09 +0000

    cod

--- Custom_hooks_APi/custom_hook_api/src/App.jsx
+++ Custom_hooks_APi/custom_hook_api/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import UsersList from './userList'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <UsersList/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
